481731ce0b6ec941ad2ef3707ddafb1c681dfcfa
Hanna Shephard <hanna_shephard@brown.edu>
2019-11-19 16:00:52 -0500

    r script

---
+++ ESLS r script.R
@@ -0,0 +1,11 @@
+install.packages("dplyr")
+library(dplyr)
+
+presurvey<-as_tibble(Eat_Smart_Live_Strong_Data_Pre)
+presurvey
+postsurvey<-as_tibble(Eat_Smart_Live_Strong_Data_Post)
+postsurvey
+
+
+
+
