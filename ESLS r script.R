279cad2c0087312af70065f1e642bb8a76cf82aa
Hanna Shephard <hanna_shephard@brown.edu>
2019-11-25 16:15:27 -0800

    read excel workbooks

--- ESLS r script.R
+++ ESLS r script.R
@@ -1,8 +1,12 @@
 install.packages("dplyr")
 library(dplyr)
+install.packages("readxl")
+library(readxl)
 
+Eat_Smart_Live_Strong_Data_Pre <-read_excel("/Users/hannashephard/Desktop/Eat Smart Live Strong Data.xlsx", sheet = 1)
 presurvey<-as_tibble(Eat_Smart_Live_Strong_Data_Pre)
 presurvey
+Eat_Smart_Live_Strong_Data_Post <-read_excel("/Users/hannashephard/Desktop/Eat Smart Live Strong Data.xlsx", sheet = 2)
 postsurvey<-as_tibble(Eat_Smart_Live_Strong_Data_Post)
 postsurvey
 
