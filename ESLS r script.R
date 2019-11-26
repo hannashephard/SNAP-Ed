4e22a52ae547f95fc0df1f8c75a0346938c77de1
Hanna Shephard <hanna_shephard@brown.edu>
2019-11-26 07:34:23 -0800

    reclassify missings and join tbls

--- ESLS r script.R
+++ ESLS r script.R
@@ -10,6 +10,16 @@ Eat_Smart_Live_Strong_Data_Post <-read_excel("/Users/hannashephard/Desktop/Eat S
 postsurvey<-as_tibble(Eat_Smart_Live_Strong_Data_Post)
 postsurvey
 
+presurvey_1<-presurvey %>%
+  na_if(99)
 
+postsurvey_1<-postsurvey %>%
+  na_if(99)
 
+prepost<-inner_join(presurvey_1, postsurvey_1, by = "ID")
 
+prepost_1<-full_join(presurvey_1, postsurvey_1, by = "ID")
+
+prepost_2<-right_join(presurvey_1, postsurvey_1, by = "ID")
+
+prepost_3<-left_join(presurvey_1, postsurvey_1, by = "ID")
