1f556b5323d022dd70fe79086d92c63f3fb73983
Hanna Shephard <hanna_shephard@brown.edu>
2019-12-03 08:48:39 -0800

    Cooking matters data

---
+++ CM R Script.R
@@ -0,0 +1,3 @@
+#Load in excel workbooks and transform to tibbles.
+Cooking_Matters_Combined_Data <-read_excel("/Users/hannashephard/Desktop/SNAP-Ed_CM_2019.xlsx", sheet = 1)
+CM_data<-as_tibble(Cooking_Matters_Combined_Data)
