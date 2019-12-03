3e51d9754e0e31477d3d4996d9c4262a842355d5
Hanna Shephard <hanna_shephard@brown.edu>
2019-12-03 09:02:29 -0800

    code for frequencies

--- CM R Script.R
+++ CM R Script.R
@@ -1,3 +1,7 @@
 #Load in excel workbooks and transform to tibbles.
 Cooking_Matters_Combined_Data <-read_excel("/Users/hannashephard/Desktop/SNAP-Ed_CM_2019.xlsx", sheet = 1)
 CM_data<-as_tibble(Cooking_Matters_Combined_Data)
+
+#Run frequencies.
+lapply(CM_data, function(x) prop.table(table(x)))
+
